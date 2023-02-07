b5e65ad60db34fcea8578274f15fc8400ba11e83
Marco Aurelio Salazar <90633967+marcusats@users.noreply.github.com>
2023-02-07 10:25:10 -0500

    logos

--- pages/index.js
+++ pages/index.js
@@ -30,10 +30,10 @@ export default function Home() {
           </div>
           <p>SmartMinter is a web application that allows you to create your own NFTs. Please use a desktop or laptop to access the application.</p> 
           <div className={styles['images']}>
-            <a href='https://twitter.com/CabalLabs' target="_blank" rel="noopener noreferrer" ><img src="/Assets/twitter.png" alt="mobile"/></a>
-           <a href='https://discord.gg/qJ3FBSQQ8T' target="_blank" rel="noopener noreferrer" ><img src="/Assets/discord.png" alt="mobile"/></a>
-           <a href='https://www.youtube.com/channel/UC0KhSOQYRqnp0Bo8Ew0Kd3g' target="_blank" rel="noopener noreferrer" ><img src="/Assets/youtube.png" alt="mobile"/></a>
-            <a href='https://www.tiktok.com/@caballabs' target="_blank" rel="noopener noreferrer"><img src="/Assets/tik-tok.png" alt="mobile"/></a>
+            <a href='https://twitter.com/CabalLabs' target="_blank" rel="noopener noreferrer" ><img  className={styles["twitter"]} src="/Assets/twitter.png" alt="mobile"/></a>
+           <a href='https://discord.gg/qJ3FBSQQ8T' target="_blank" rel="noopener noreferrer" ><img className={styles["dicord"]} src="/Assets/discord.png" alt="mobile"/></a>
+           <a href='https://www.youtube.com/channel/UC0KhSOQYRqnp0Bo8Ew0Kd3g' target="_blank" rel="noopener noreferrer" ><img className={styles['youtube']} src="/Assets/youtube.png" alt="mobile"/></a>
+            <a href='https://www.tiktok.com/@caballabs' target="_blank" rel="noopener noreferrer"><img className={styles["tiktok"]} src="/Assets/tik-tok.png" alt="mobile"/></a>
           </div>
         </div>
         <div className={styles['mobile-footer']} >
